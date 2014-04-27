28dd525e0987e2cfec4a94902b415d05fa321580
pbosio <bosio.patricio@gmail.com>
2014-04-27 11:54:53 -0300

    fixes
    *fixed settings not being read
    *changed layout
    +added Spanish translation
    *minSdk is now 14

--- src/ar/pbosio/whatsappquiethours/MainActivity.java
+++ src/ar/pbosio/whatsappquiethours/MainActivity.java
@@ -1,44 +1,60 @@
 package ar.pbosio.whatsappquiethours;
 
+import ar.pbosio.whatsappquiethours.R;
+
+import android.app.ActionBar;
 import android.app.Activity;
 import android.content.ComponentName;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.preference.Preference;
-import android.preference.PreferenceActivity;
+import android.preference.PreferenceFragment;
+import android.view.MenuItem;
 
+public class MainActivity extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 
-public class MainActivity extends PreferenceActivity {
-	@Override
-	protected void onPostCreate(Bundle savedInstanceState) {
-		super.onPostCreate(savedInstanceState);
+        if (savedInstanceState == null)
+            getFragmentManager().beginTransaction().replace(android.R.id.content, new PrefsFragment()).commit();
+        
+        ActionBar b = getActionBar();
+        b.setHomeButtonEnabled(true);
 
-		setupSimplePreferencesScreen();
-	}
+    }
+    
+    @Override 
+    public boolean onOptionsItemSelected (MenuItem item) { 
+    	if(item.getItemId() == android.R.id.home) {
+    		onBackPressed();
+    	}
+		return true;
+    }
 
-	@SuppressWarnings("deprecation")
-	private void setupSimplePreferencesScreen() {
-		addPreferencesFromResource(R.xml.preferences);
-		
-		final Activity activity = this;
-		final Preference pref_hide_launcher = this.findPreference("pref_hide_launcher");
-		
-        pref_hide_launcher.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
+    public static class PrefsFragment extends PreferenceFragment {
 
-            @Override
-            public boolean onPreferenceChange(Preference preference, Object newValue) {
-                PackageManager p = activity.getPackageManager();
-                int state = (Boolean) newValue ? PackageManager.COMPONENT_ENABLED_STATE_DISABLED : PackageManager.COMPONENT_ENABLED_STATE_ENABLED;
-                final ComponentName alias = new ComponentName(activity, "com.pbosio.whatsappquiethours.MainActivity-Alias");
-                p.setComponentEnabledSetting(alias, state, PackageManager.DONT_KILL_APP);
-                return true;
-            }
-        });  		
-		
-	}
+        @SuppressWarnings("deprecation")
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
 
-	@Override
-	public boolean onIsMultiPane() {
-		return false;
-	}
+            getPreferenceManager().setSharedPreferencesMode(MODE_WORLD_READABLE);
+            addPreferencesFromResource(R.xml.preferences);
+            final Activity activity = getActivity();
+            
+            final Preference pref_hide_launcher = this.findPreference("pref_hide_launcher");
+            pref_hide_launcher.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
+
+                @Override
+                public boolean onPreferenceChange(Preference preference, Object newValue) {
+                    PackageManager p = activity.getPackageManager();
+                    int state = (Boolean) newValue ? PackageManager.COMPONENT_ENABLED_STATE_DISABLED : PackageManager.COMPONENT_ENABLED_STATE_ENABLED;
+                    final ComponentName alias = new ComponentName(activity, "com.pbosio.whatsappquiethours.MainActivity-Alias");
+                    p.setComponentEnabledSetting(alias, state, PackageManager.DONT_KILL_APP);
+                    return true;
+                }
+            });            
+        }
+    }
 }
